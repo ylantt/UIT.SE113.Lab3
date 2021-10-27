2fac22e643401d1c913594e17f4d2f5ee7ef6e7f
Lan Truong <truongylan2000@gmail.com>
2021-10-27 10:23:00 +0700

    IVD1-IVD5

---
+++ test/IVD.test.js
@@ -0,0 +1,58 @@
+import { Selector } from 'testcafe';
+
+fixture `Check In valid date function`
+    .page`http://localhost:8080/TestingDateApp/UI.html`;
+
+test(`IVD01 - Should_Invalid_When_InputDayIs_1_InputMonthIs_0_InputYearIs_2020`, async t=>{
+
+   await t
+        .typeText('#day', '1')
+        .typeText('#month', '0')
+        .typeText('#year', '2020')
+        .click('#checkValid')
+       // Use the assertion to check if the actual result is equal to the expected one
+        .expect(Selector('#Result').innerText).eql('false');
+})
+
+test(`IVD02 - Should_Invalid_When_InputDayIs_10_InputMonthIs_13_InputYearIs_2021`, async t=>{
+
+   await t
+        .typeText('#day', '10')
+        .typeText('#month', '13')
+        .typeText('#year', '2021')
+        .click('#checkValid')
+       // Use the assertion to check if the actual result is equal to the expected one
+        .expect(Selector('#Result').innerText).eql('false');
+})
+
+test(`IVD03 - Should_Invalid_When_InputDayIs_0_InputMonthIs_10_InputYearIs_2020`, async t=>{
+
+   await t
+        .typeText('#day', '0')
+        .typeText('#month', '10')
+        .typeText('#year', '2020')
+        .click('#checkValid')
+       // Use the assertion to check if the actual result is equal to the expected one
+        .expect(Selector('#Result').innerText).eql('false');
+})
+
+test(`IVD04 - Should_Invalid_When_InputDayIs_29_InputMonthIs_2_InputYearIs_2021`, async t=>{
+
+   await t
+        .typeText('#day','29')
+        .typeText('#month', '2')
+        .typeText('#year', '2021')
+        .click('#checkValid')
+       // Use the assertion to check if the actual result is equal to the expected one
+        .expect(Selector('#Result').innerText).eql('false');
+})
+test(`IVD05 - Should_Valid_When_InputDayIs_28_InputMonthIs_2_InputYearIs_2021`, async t=>{
+
+    await t
+         .typeText('#day','28')
+         .typeText('#month', '2')
+         .typeText('#year', '2021')
+         .click('#checkValid')
+        // Use the assertion to check if the actual result is equal to the expected one
+         .expect(Selector('#Result').innerText).eql('true');
+ })
